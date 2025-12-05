2c6873dca8bda9678c760a4ad2f2a41328f0378e
Carrison <carlos97181034535@gmail.com>
2025-12-05 15:38:15 +0100

    add accepted domain for CORS

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ DB.authenticate()
   .then(() => console.log('Base de datos conectada'))
   .catch((error) => console.log('Error al conectar la base de datos: ', error))
 
-const acceptedDomains = ['http://localhost:5173']
+const acceptedDomains = ['http://localhost:5173', 'https://biblioteca-juegos-carrison.netlify.app']
 
 const corsOptions = {
   origin: (origin, callback) => {
