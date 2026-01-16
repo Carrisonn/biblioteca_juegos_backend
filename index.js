ae345e498e3c8de8aae2e5352fddb28c50fde102
Carrison <carlos97181034535@gmail.com>
2026-01-16 15:32:39 +0100

    refactor: reorganize database connection and CORS options in index.js

--- index.js
+++ index.js
@@ -2,28 +2,15 @@ import express from 'express'
 import cors from 'cors'
 import { router } from './routes/router.js'
 import { DB } from './config/DB.js'
+import { corsOptions } from './config/cors.js'
 
 const app = express()
 
-DB.authenticate()
-  .then(() => console.log('Base de datos conectada'))
-  .catch((error) => console.log('Error al conectar la base de datos: ', error))
+const PORT = process.env.PORT ?? 8080
 
-const acceptedDomains = ['http://localhost:5173', 'https://biblioteca-juegos-carrison.netlify.app']
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || acceptedDomains.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error('No permitido por CORS'))
-    }
-  }
-}
+await DB.authenticate()
 
 app.use(cors(corsOptions))
-
-const PORT = process.env.PORT ?? 8080
 
 app.use(express.json())
 
