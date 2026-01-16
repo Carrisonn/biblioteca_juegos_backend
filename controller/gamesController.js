e9547fc938c45a3e86bba5423b0f3576cfd4f0f7
Carrison <carlos97181034535@gmail.com>
2026-01-16 15:31:56 +0100

    refactor: Improve validation and error handling in game finding, creating, deleting, and editing features

--- controller/gamesController.js
+++ controller/gamesController.js
@@ -12,11 +12,13 @@ export async function getGames(req, res) {
 }
 
 export async function searchGame(req, res) {
+  const { game } = req.params
+  if (!game) return res.status(400).json({ errorMessage: 'Faltan datos en el formulario' })
+
   try {
-    const { game } = req.params
-    if (!game) return res.status(400).json({ errorMessage: 'Faltan datos en el formulario' })
     const games = await GameModel.findAll({ where: { game: { [Op.like]: `%${game}%` } } })
-    if (!games.length) return res.status(404).json({ errorMessage: `El juego "${game}" no existe` })
+    if (games.length === 0) return res.status(404).json({ errorMessage: `El juego "${game}" no existe` })
+
     res.status(200).json({ games })
   } catch (error) {
     console.log(error)
@@ -25,11 +27,13 @@ export async function searchGame(req, res) {
 }
 
 export async function createGame(req, res) {
+  const { game, state } = req.body
+  if (!game || !state) return res.status(400).json({ errorMessage: 'Faltan datos en el formulario' })
+
   try {
-    const { game, state } = req.body
-    if (!game || !state) return res.status(400).json({ errorMessage: 'Faltan datos en el formulario' })
     const gameExist = await GameModel.findOne({ where: { game } })
     if (gameExist) return res.status(409).json({ errorMessage: `El juego "${game}" ya esta en la lista` })
+
     const newGame = await GameModel.create({ game, state })
     res.status(200).json({ newGame, successMessage: `${game} agregado a la lista 🎉` })
   } catch (error) {
@@ -39,11 +43,13 @@ export async function createGame(req, res) {
 }
 
 export async function deleteGame(req, res) {
+  const { id } = req.params
+  if (!id) return res.status(400).json({ errorMessage: 'Falta el ID para borrar el juego' })
+
   try {
-    const { id } = req.params
-    if (!id) return res.status(400).json({ errorMessage: 'Falta el ID para borrar el juego' })
     const gameToDelete = await GameModel.findByPk(id)
     if (!gameToDelete) return res.status(404).json({ errorMessage: 'No se ha podido borrar el juego o ya ha sido borrado' })
+
     await gameToDelete.destroy()
     res.status(200).json({ gameToDelete, successMessage: 'Juego borrado de la lista' })
   } catch (error) {
@@ -53,11 +59,13 @@ export async function deleteGame(req, res) {
 }
 
 export async function editGame(req, res) {
+  const { id, game, state } = req.body
+  if (!id || !game || !state) return res.status(400).json({ errorMessage: 'Faltan datos en el formulario' })
+
   try {
-    const { id, game, state } = req.body
-    if (!id || !game || !state) return res.status(400).json({ errorMessage: 'Faltan datos en el formulario' })
     const [affectedRows] = await GameModel.update({ game, state }, { where: { id } })
     if (!affectedRows) return res.status(404).json({ errorMessage: 'Hubo un problema al guardar cambios' })
+
     const updatedGame = await GameModel.findOne({ where: { id } })
     res.status(200).json({ updatedGame, successMessage: 'Juego actualizado 🎉' })
   } catch (error) {
