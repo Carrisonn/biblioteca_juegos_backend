a87313902a7840cc2ba0dae6b5635022c5b258ce
Carrison <carlos97181034535@gmail.com>
2026-02-24 15:25:50 +0100

    refactor: define game model with unique and non-null constraints

--- model/GameModel.js
+++ model/GameModel.js
@@ -2,6 +2,13 @@ import { DataTypes } from 'sequelize'
 import { DB } from '../config/DB.js'
 
 export const GameModel = DB.define('games', {
-  game: { type: DataTypes.STRING },
-  state: { type: DataTypes.STRING }
+  game: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+  state: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
 })
